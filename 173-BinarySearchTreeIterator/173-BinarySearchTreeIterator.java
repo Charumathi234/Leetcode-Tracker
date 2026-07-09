544542facda82a461354f6b92a5f69cdb9762c96
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:09:47 +0530

    Create 173-BinarySearchTreeIterator

---
+++ 173-BinarySearchTreeIterator/173-BinarySearchTreeIterator.java
@@ -0,0 +1,48 @@
+// Last updated: 7/9/2026, 3:09:47 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.Stack;
+
+class BSTIterator {
+
+    private Stack<TreeNode> stack = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushLeft(root);
+    }
+
+    private void pushLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if (node.right != null) {
+            pushLeft(node.right);
+        }
+
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
