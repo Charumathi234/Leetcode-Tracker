13be0abd293aef3cb7e2b76efe6780ee05ec61b4
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:07:00 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:07:00 PM
+class Solution {
+    public int search(int[] nums, int target) {
+       int low=0,high=nums.length-1;
+       int mid;
+       while(low<=high){
+        mid =(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]>target){
+            high=mid-1;
+        }else {
+            low=mid+1;
+        }
+       }
+       return -1;
+    }
+}
