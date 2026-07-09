25e409490df44d9b29c5366aaa157ce0ce92b7db
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:08:03 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 3:08:02 PM
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num==0){
+            return 0;
+        }
+        return num%9==0?9:num%9;
+    }
+};
