dc58cd861c47596a47bc08c29815b7b4cde14e0b
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:08:24 +0530

    Create 218-TheSkylineProblem

---
+++ 218-TheSkylineProblem/218-TheSkylineProblem.java
@@ -0,0 +1,55 @@
+// Last updated: 7/9/2026, 3:08:24 PM
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+
+        List<int[]> events = new ArrayList<>();
+        for (int[] b : buildings) {
+            events.add(new int[]{b[0], -b[2]}); 
+            events.add(new int[]{b[1], b[2]});  
+        }
+
+        // Sort events
+        Collections.sort(events, (a, b) -> {
+            if (a[0] != b[0])
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Map<Integer, Integer> count = new HashMap<>();
+
+        pq.offer(0);
+        count.put(0, 1);
+
+        int prevHeight = 0;
+
+        for (int[] event : events) {
+
+            int x = event[0];
+            int h = event[1];
+
+            if (h < 0) { 
+                h = -h;
+                pq.offer(h);
+                count.put(h, count.getOrDefault(h, 0) + 1);
+            } else {
+                count.put(h, count.get(h) - 1);
+
+                while (!pq.isEmpty() && count.getOrDefault(pq.peek(), 0) == 0) {
+                    pq.poll();
+                }
+            }
+
+            int currHeight = pq.peek();
+
+            if (currHeight != prevHeight) {
+                result.add(Arrays.asList(x, currHeight));
+                prevHeight = currHeight;
+            }
+        }
+
+        return result;
+    }
+}
