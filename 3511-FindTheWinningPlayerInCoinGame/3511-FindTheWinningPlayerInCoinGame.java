a5ae1449d79fc3843e9f950b91ef8f79520a5024
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:05:22 +0530

    Create 3511-FindTheWinningPlayerInCoinGame

---
+++ 3511-FindTheWinningPlayerInCoinGame/3511-FindTheWinningPlayerInCoinGame.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 3:05:21 PM
+class Solution {
+    public String winningPlayer(int x, int y) {
+        int turn =1;
+        while(x>=1 && y>=4){
+            x-=1;
+            y-=4;
+            turn++;
+        }
+        return (turn%2==0)? "Alice":"Bob";
+    }
+}
