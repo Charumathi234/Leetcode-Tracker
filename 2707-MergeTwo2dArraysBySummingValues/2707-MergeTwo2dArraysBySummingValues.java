c476a1481407a565ff55918af9a9dfbf1f20b6cd
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:05:41 +0530

    Create 2707-MergeTwo2dArraysBySummingValues

---
+++ 2707-MergeTwo2dArraysBySummingValues/2707-MergeTwo2dArraysBySummingValues.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 3:05:41 PM
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+         
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for(int[] n : nums1){
+            map.put(n[0], map.getOrDefault(n[0],0) + n[1]);
+        }
+
+        for(int[] n : nums2){
+            map.put(n[0], map.getOrDefault(n[0],0) + n[1]);
+        }
+
+        int[][] result = new int[map.size()][2];
+        int i = 0;
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            result[i][0] = entry.getKey();
+            result[i][1] = entry.getValue();
+            i++;
+        }
+
+        return result; 
+    }
+}
