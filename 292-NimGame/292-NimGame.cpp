b788be0b715948cb8f7122ab49f9636252650f9d
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-09 15:07:55 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 3:07:54 PM
+class Solution {
+public:
+    bool canWinNim(int n) {
+       if(n%4==0){
+        return false;
+       }else {
+        return true;
+       }
+    }
+};
