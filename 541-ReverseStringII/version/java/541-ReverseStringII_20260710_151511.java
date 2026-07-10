184b1bf44dfc3204bf07c85fbdc570dd19586489
Charumathi M <charumathicharumathi234@gmail.com>
2026-07-10 15:15:12 +0530

    Create 541-ReverseStringII

---
+++ 541-ReverseStringII/version/java/541-ReverseStringII_20260710_151511.java
@@ -0,0 +1,33 @@
+// Last updated: 7/10/2026, 3:15:11 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int minDepth(TreeNode root) {
+18        if (root == null) {
+19            return 0;
+20        }
+21
+22        if (root.left == null) {
+23            return 1 + minDepth(root.right);
+24        }
+25
+26        if (root.right == null) {
+27            return 1 + minDepth(root.left);
+28        }
+29
+30        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+31    }
+32}
